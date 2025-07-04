0325cf006ecd0fc0d8d68c7e0cf5765c9dfc5588
U071243 <u071243@bca.co.id>
2025-07-04 08:22:45 +0700

    penyetaraan dengan code instructor

--- db/query/account.sql
+++ db/query/account.sql
@@ -19,3 +19,14 @@ UPDATE accounts SET balance = $2 WHERE id = $1 RETURNING *;
 -- name: DeleteAccount :exec
 
 DELETE FROM accounts WHERE id = $1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts 
+WHERE id = $1 
+FOR UPDATE;
+
+-- name: AddAccountBalance :one
+UPDATE accounts 
+SET balance = balance + $2 
+WHERE id = $1 
+RETURNING *;
--- db/store.go
+++ db/store.go
@@ -3,17 +3,16 @@ package db
 import (
 	"context"
 	"database/sql"
-	"fmt"
 )
 
 type Store interface {
-	Querier()
 	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+	Querier
 }
 
 type SQLStore struct {
-	*Queries
 	db *sql.DB
+	*Queries
 }
 
 func NewStore(db *sql.DB) Store {
@@ -21,49 +20,4 @@ func NewStore(db *sql.DB) Store {
 		db:      db,
 		Queries: New(db),
 	}
-}
-
-func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
-	tx, err := store.db.BeginTx(ctx, nil)
-	if err != nil {
-		return err
-	}
-	q := New(tx)
-
-	err = fn(q)
-	if err != nil {
-		errRb := tx.Rollback()
-		if errRb != nil {
-			return fmt.Errorf("tx err: %v, rollback err: %v", err, errRb)
-		}
-		return tx.Commit()
-	}
-}
-
-type TransferTxParams struct {
-	FromAccountID int64
-	ToAccountID   int64
-	Amount        int64
-}
-
-type TransferTxResult struct {
-	Transfer Transfer
-}
-
-func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-	var result TransferTxResult
-
-	err := store.execTx(ctx, func(q *Queries) error {
-		transfer, err := q.CreateTransfer(ctx, CreateTransferParams{
-			FromAccountID: arg.FromAccountID,
-			ToAccountID:   arg.ToAccountID,
-			Amount:        arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-		result.Transfer = transfer
-		return nil
-	})
-	return result, err
 }
